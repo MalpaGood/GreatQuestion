90423b80464985d787396d95b0fc65b73ddad238
MalpaGood <gooduncle4@gmail.com>
2023-10-20 17:02:26 +0200

    Update

--- script.js
+++ script.js
@@ -1,8 +1,22 @@
+const container = document.querySelector(".button-container");
 const oldButtons = document.querySelectorAll(".old-button");
+
 const newButtonsGroups = [
-    ["Nowy Przycisk A1", "Nowy Przycisk A2", "Nowy Przycisk A3"],
-    ["Nowy Przycisk B1", "Nowy Przycisk B2", "Nowy Przycisk B3"],
-    ["Nowy Przycisk C1", "Nowy Przycisk C2", "Nowy Przycisk C3"],
+    [
+        { text: "Nowy Przycisk A1", url: "url_podstrony_A1.html" },
+        { text: "Nowy Przycisk A2", url: "url_podstrony_A2.html" },
+        { text: "Nowy Przycisk A3", url: "url_podstrony_A3.html" },
+    ],
+    [
+        { text: "Nowy Przycisk B1", url: "url_podstrony_B1.html" },
+        { text: "Nowy Przycisk B2", url: "url_podstrony_B2.html" },
+        { text: "Nowy Przycisk B3", url: "url_podstrony_B3.html" },
+    ],
+    [
+        { text: "Nowy Przycisk C1", url: "url_podstrony_C1.html" },
+        { text: "Nowy Przycisk C2", url: "url_podstrony_C2.html" },
+        { text: "Nowy Przycisk C3", url: "url_podstrony_C3.html" },
+    ],
 ];
 
 oldButtons.forEach((button, index) => {
@@ -10,27 +24,76 @@ oldButtons.forEach((button, index) => {
         // Ukryj stare przyciski
         oldButtons.forEach((btn) => {
             btn.style.opacity = "0";
-            btn.style.transform = "translateY(20px)";
-            setTimeout(() => {
-                btn.style.visibility = "hidden";
-            }, 500);
+            btn.style.transform = "translateY(0px)";
         });
 
-        // Ustal tekst dla nowych przycisków
-        const newButtonGroup = newButtonsGroups[index];
-        newButtonGroup.forEach((text, idx) => {
-            const newButton = document.querySelectorAll(".new-button")[idx];
-            newButton.textContent = text;
-        });
+        // Po zakończeniu animacji ukrywania starych przycisków:
+        setTimeout(() => {
+            // Ustaw stare przyciski na 'display: none'
+            oldButtons.forEach((btn) => {
+                btn.style.display = "none";
+            });
 
-        // Pokaż nowe przyciski
-        const newButtons = document.querySelectorAll(".new-button");
-        newButtons.forEach((btn) => {
-            btn.style.visibility = "visible";
-            setTimeout(() => {
-                btn.style.opacity = "1";
-                btn.style.transform = "translateY(0px)";
-            }, 10);
-        });
+            // Usuń poprzednie nowe przyciski (jeśli istnieją)
+            const existingNewButtons = document.querySelectorAll(".new-button");
+            existingNewButtons.forEach((btn) => btn.remove());
+
+            // Dodaj nowe przyciski
+            const newButtonGroup = newButtonsGroups[index];
+            newButtonGroup.forEach((data) => {
+                const newButton = document.createElement("button");
+                newButton.classList.add("new-button", "new-button-entering");
+                newButton.textContent = data.text;
+                newButton.setAttribute("data-url", data.url);
+
+                newButton.addEventListener("click", function () {
+                    const url = this.getAttribute("data-url");
+                    if (url) {
+                        window.location.href = url;
+                    }
+                });
+
+                container.appendChild(newButton);
+
+                setTimeout(() => {
+                    newButton.classList.remove("new-button-entering");
+                }, 10);
+            });
+
+            // Dodaj przycisk cofania
+            const backButton = document.createElement("button");
+            backButton.classList.add("back-button");
+            backButton.textContent = "Cofnij do początkowych przycisków";
+            backButton.addEventListener("click", function () {
+                // Ukryj nowe przyciski, w tym przycisk cofania
+                const allNewButtons = document.querySelectorAll(".new-button");
+                allNewButtons.forEach((btn) => {
+                    btn.style.opacity = "0";
+                });
+
+                // Po zakończeniu animacji ukrywania nowych przycisków:
+                setTimeout(() => {
+                    allNewButtons.forEach((btn) => {
+                        btn.remove();
+                    });
+
+                    // Usuń przycisk "Cofnij"
+                    const backButtonElement =
+                        document.querySelector(".back-button");
+                    if (backButtonElement) {
+                        backButtonElement.remove();
+                    }
+
+                    // Pokaż stare przyciski
+                    oldButtons.forEach((btn) => {
+                        btn.style.display = "";
+                        btn.style.opacity = "1";
+                        btn.style.transform = "translateY(0px)";
+                    });
+                }, 400); // Dopasuj czas do długości animacji ukrywania nowych przycisków
+            });
+
+            container.appendChild(backButton);
+        }, 400); // Dopasuj czas do długości animacji ukrywania starych przycisków
     });
 });
